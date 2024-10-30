c08738c07f2224336344749443b7d3e4a5078572
Furkan Hoca <furkanhocavektorel@gmail.com>
2024-10-30 20:44:17 +0300

    test baslik

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!"+ "asadsasda"+44444);
+            Console.WriteLine("asjkdasasdghadhgas");
         }
     }
 }
